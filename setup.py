266bded588962f8c6085c7103818307abe2a4f93
burner <burner@localhost.localdomain>
2018-08-08 00:41:59 -0700

    0.1.3 bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="badgebtle",
-    version="0.1.2",
+    version="0.1.3",
     author="Robert Bost",
     description="DEF CON badge bluetooth library",
     long_description=long_description,
